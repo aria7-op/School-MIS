a4393ee1dbab7f681577023776650a3e7c728601
School MIS Developer <developer@school-mis.com>
2026-01-04 21:40:27 -0800

    Update superadminController.js

--- controllers/superadminController.js
+++ controllers/superadminController.js
@@ -12,7 +12,7 @@ const convertBigIntToString = (obj) => {
   if (obj === null || obj === undefined) return obj;
   if (typeof obj === 'bigint') return obj.toString();
   if (Array.isArray(obj)) return obj.map(convertBigIntToString);
-  if (typeof obj === 'object') {
+  if (typeof obj === 'object') {  
     const converted = {};
     for (const key in obj) {
       converted[key] = convertBigIntToString(obj[key]);
